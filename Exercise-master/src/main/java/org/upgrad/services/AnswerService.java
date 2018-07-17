baa32f71a78b7d2ca04773fc4e2b3d620c1bbf83
Chandra Prakash Tekam <40147877+cptekam@users.noreply.github.com>
2018-07-17 12:11:32 +0530

    added  Integer deleteAnswer (Integer answerId);

--- Exercise-master/src/main/java/org/upgrad/services/AnswerService.java
+++ Exercise-master/src/main/java/org/upgrad/services/AnswerService.java
@@ -5,9 +5,12 @@ import org.upgrad.model.Answer;
 import java.util.List;
 
 public interface AnswerService {
+    
     List <Answer> getAllAnswersToQuestion(Integer questionId);
 
     List <Answer> getAllAnswersByLikes(Integer questionId);
 
     Integer findUserIdfromAnswer(Integer answerId);
+    
+    Integer deleteAnswer (Integer answerId);
 }
